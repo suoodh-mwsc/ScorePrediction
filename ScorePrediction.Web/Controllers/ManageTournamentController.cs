bd1a2875ee8f0c1e69983fefbc9fc9bf5962a1bf
Mohamed Suoodh <DU10031222@mwsc.com.mv>
2022-11-16 08:12:50 +0500

    changes

---
+++ ScorePrediction.Web/Controllers/ManageTournamentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScorePrediction.Web.Models;
+using ScorePrediction.Web.Models.Dto;
+
+namespace ScorePrediction.Web.Controllers
+{
+    public class ManageTournamentController : BaseController
+    {
+        private readonly ScorePredictionDbContext _dbContext;
+
+        public ManageTournamentController(ScorePredictionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult TournamentList(Guid? id)
+        {
+            DateTime todayDate = DateTime.Now.Date;
+            var objList = new ManageTournamentDto();
+            var DbF = Microsoft.EntityFrameworkCore.EF.Functions;
+
+           // objList.TournamentId = id;
+
+            objList.SelectedTournament = _dbContext.Tournaments.Find(id);
+
+            objList.TournamentTeams = _dbContext.Teams
+                .Include(m => m.Tournament)
+                .Where(e => e.Tournament.Id == id && e.DeletedOn == null)
+                .OrderBy(x => x.CreatedOn).ToList();
+
+
+            objList.TournamentMatches = _dbContext.Matches
+                .Include(m => m.AwayTeam)
+                .Include(m => m.HomeTeam)
+                .Include(m => m.Tournament)
+                .Where(e => e.Tournament.Id == id
+                        && e.DeletedOn == null
+                        && e.PublishedOn.HasValue
+                        && DbF.DateDiffDay(todayDate, e.StartOn) >= 1)
+                .OrderBy(x => x.StartOn).ToList();
+
+            return View(objList);
+        }
+
+        public IActionResult ManageTournamentList()
+        {
+            var objectList = _dbContext.Tournaments.ToList();
+            return View(objectList);
+        }
+    }
+}
